785a0c6fe31846cec29d73158d0efd4829b0face
Takafumi Arakaki <aka.tkf@gmail.com>
2022-03-26 03:18:55 -0700

    Enable deploydocs (#3)

--- docs/make.jl
+++ docs/make.jl
@@ -1,15 +1,32 @@
-using Documenter
 using DocumentationOverview
+using Documenter
 
 makedocs(
-    sitename = "DocumentationOverview",
+    sitename = "Try",
     format = Documenter.HTML(),
-    modules = [DocumentationOverview]
+    modules = [DocumentationOverview],
+    strict = [
+        :autodocs_block,
+        :cross_references,
+        :docs_block,
+        :doctest,
+        :eval_block,
+        :example_block,
+        :footnote,
+        :linkcheck,
+        :meta_block,
+        # :missing_docs,
+        :parse_error,
+        :setup_block,
+    ],
+    # Ref:
+    # https://juliadocs.github.io/Documenter.jl/stable/lib/public/#Documenter.makedocs
 )
 
-# Documenter can also automatically deploy documentation to gh-pages.
-# See "Hosting Documentation" and deploydocs() in the Documenter manual
-# for more information.
-#=deploydocs(
-    repo = "<repository url>"
-)=#
+deploydocs(
+    repo = "github.com/tkf/DocumentationOverview.jl",
+    devbranch = "main",
+    push_preview = true,
+    # Ref:
+    # https://juliadocs.github.io/Documenter.jl/stable/lib/public/#Documenter.deploydocs
+)
