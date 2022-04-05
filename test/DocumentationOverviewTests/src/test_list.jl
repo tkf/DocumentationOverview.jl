af21fbe6dd84a6652a984d28f6722d241b4c5492
Takafumi Arakaki <aka.tkf@gmail.com>
2022-04-05 00:55:27 -0700

    Run list tests (#22)
    Co-authored-by: Fredrik Bagge Carlson <baggepinnen@gmail.com>

--- test/DocumentationOverviewTests/src/test_list.jl
+++ test/DocumentationOverviewTests/src/test_list.jl
@@ -6,7 +6,7 @@ using Test
 function test_default()
     list = DocumentationOverview.list(DocumentationOverview)
     text = sprint(show, "text/plain", list)
-    @test startswith(text, "* ")
+    @test occursin("•  ", text)
     @test occursin("DocumentationOverview.table", text)
     @test occursin("DocumentationOverview.find", text)
     @test occursin("DocumentationOverview.API", text)
