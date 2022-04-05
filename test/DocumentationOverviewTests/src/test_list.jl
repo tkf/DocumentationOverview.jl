13bfe59795433b76fb10881c6fec06aec301f0fe
Takafumi Arakaki <aka.tkf@gmail.com>
2022-04-05 03:52:04 -0400

    Test list printing

--- test/DocumentationOverviewTests/src/test_list.jl
+++ test/DocumentationOverviewTests/src/test_list.jl
@@ -6,6 +6,7 @@ using Test
 function test_default()
     list = DocumentationOverview.list(DocumentationOverview)
     text = sprint(show, "text/plain", list)
+    @test occursin("•  ", text)
     @test occursin("DocumentationOverview.table", text)
     @test occursin("DocumentationOverview.find", text)
     @test occursin("DocumentationOverview.API", text)
